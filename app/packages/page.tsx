69ec7fc03bd53952a582240707cf5572daa78903
evan tu <59384760+evanjt06@users.noreply.github.com>
2024-10-26 14:16:05 -0500

    User type

--- app/packages/page.tsx
+++ app/packages/page.tsx
@@ -7,10 +7,22 @@ import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
 import { fetchUser } from "@/api/packages";
 
+interface User {
+    id: string;
+    email: string;
+    name: string;
+    account_created: string;
+    can_add_and_delete_packages: boolean;
+    can_claim_packages: boolean;
+    can_administrate_users: boolean;
+    user_type: string;
+  }
+
+
 export default function Packages() {
 
     const [loading, setLoading] = useState(true)
-    const [user, setUser] = useState([])
+    const [user, setUser] = useState<User | null>(null)
 //   const {
 //     data: { user },
 //   } = await supabase.auth.getUser();
@@ -37,7 +49,7 @@ export default function Packages() {
     <div className="flex-1 w-full flex flex-col gap-12">
         {loading ? "Loading..." :  
       <div>
-        hi {user.name}
+        hi {user?.name}
         </div>
         }
     </div>
