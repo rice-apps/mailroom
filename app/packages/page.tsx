fbbfdf88946c033aeb7883f770b0c1d2747ef23f
fernando <fernandogonzalezcruz2004@gmail.com>
2024-11-02 13:20:24 -0500

    worked on claiming

--- app/packages/page.tsx
+++ app/packages/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
+
 import { createClient } from "@/utils/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
