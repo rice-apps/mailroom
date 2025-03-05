10fa7398faa66ddc9601e339a793825a3a551f09
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2025-03-04 22:34:09 -0600

    Add rest request log

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,7 +5,7 @@ export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the SSR package. It exchanges an auth code for the user's session.
   // https://supabase.com/docs/guides/auth/server-side/nextjs
-  console.log(JSON.stringify(request));
+  console.log("rest", JSON.stringify(request), request.url, request.origin);
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
   const origin = requestUrl.origin;
