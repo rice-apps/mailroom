e9276ac8a18eab2a4aca370472dcb9d4b701246e
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2025-03-04 22:37:48 -0600

    Add request headers

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,7 +5,7 @@ export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the SSR package. It exchanges an auth code for the user's session.
   // https://supabase.com/docs/guides/auth/server-side/nextjs
-  console.log("rest", JSON.stringify(request), request.url);
+  console.log("rest", JSON.stringify(request), request.url, request.headers, request);
   const requestUrl = new URL(request.url);
   console.log(JSON.stringify(requestUrl));
   const code = requestUrl.searchParams.get("code");
