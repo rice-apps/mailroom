bcc415465ab749a9d1fcd53cda66da81db3af114
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2025-03-10 22:33:22 -0500

    Fix cors issue and fetching user

--- supabase/functions/delete-user/index.ts
+++ supabase/functions/delete-user/index.ts
@@ -6,27 +6,42 @@
 // import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 import { createClient } from "npm:@supabase/supabase-js";
 
+const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+}
+
 Deno.serve(async (req) => {
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
+  }
+  
   const SUPABASE_URL = Deno.env.get("SUPABASE_URL");
   const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY");
   const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY ?? "");
 
-  const { user } = req.auth;
-  if (!user) {
-    return new Response("Unauthorized", { status: 401 });
+  const authHeader = req.headers.get('Authorization')!;
+  if (!authHeader) {
+    return new Response("Unauthorized", { status: 401 }, ...corsHeaders);
+  }
+  const token = authHeader.replace('Bearer ', '');
+  if (!token) {
+    return new Response("Unauthorized", { status: 401 }, ...corsHeaders);
   }
 
+  const { data } = await supabase.auth.getUser(token)
+  const user = data.user
   const { id } = user;
   let { error } = await req.supabase.from("users").delete().eq("id", id);
   if (error) {
-    return new Response(JSON.stringify(error), { status: 500 });
+    return new Response(JSON.stringify(error), { status: 500 }, ...corsHeaders);
   }
 
   error = await supabase.auth.admin.deleteUser(id).error;
   if (error) {
-    return new Response(JSON.stringify(error), { status: 500 });
+    return new Response(JSON.stringify(error), { status: 500 }, ...corsHeaders);
   }
-  return new Response(JSON.stringify({ success: true }), { status: 200 });
+  return new Response(JSON.stringify({ success: true }), { status: 200 }, ...corsHeaders);
 });
 
 /* To invoke locally:
